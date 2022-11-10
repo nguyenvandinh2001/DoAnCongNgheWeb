fc886114837492e8d3265d2e28e8a983ac0c801b
nguyenvandinh2001 <117894414+nguyenvandinh2001@users.noreply.github.com>
2022-11-10 15:04:19 +0700

    Add files via upload

---
+++ DoAnController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+
+namespace DoAnCongNgheWeb.Controllers
+{
+    public class DoAnController : Controller
+    {
+        //
+        // GET: /DoAn/
+
+        //=====Trang chủ
+
+        public ActionResult TrangChu()
+        {
+
+            return View();
+        }
+        //===============================================================ĐẶT VÉ XE
+    }
+}
